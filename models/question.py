f761cbf81d69dc95c79aacca503bc98116f5f2cc
Techteamblt <kinninkpomathias@gmail.com>
2024-05-28 11:11:58 +0100

    init add of question.py

--- models/question.py
+++ models/question.py
@@ -6,17 +6,6 @@ me.connect('your_database_name', host='your_mongodb_uri')
 
 
 class Question(Document):
-    identifiant = IntField(primary_key=True, required=True)
     content = StringField(required=True)
     
-    meta = {
-        'indexes': [
-            'identifiant'
-        ]
-    }
 
-question = Question(
-    identifiant=1,
-    content='What is the capital of France?'
-)
-question.save()
