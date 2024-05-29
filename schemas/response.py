cc9d86eae25b7301a82a96da1778eb2480b768c1
Techteamblt <kinninkpomathias@gmail.com>
2024-05-29 20:32:37 +0100

    init add of schemas

--- schemas/response.py
+++ schemas/response.py
@@ -8,13 +8,13 @@ class ResponseBase(BaseModel):
     user_id: UserResponse
     id_link: str
     status: str
-    id_personality: int
+    id_personality: str
     statistique: Dict[str, bool]
     content: Dict[str, bool]
     date: str
 
 class ResponseCreate(BaseModel):
-    user_id : UserResponse
+    user_id : str = Field(..., alias='user_id', example="60d5f446f1b8e6c7b4efddf3")
     
 
 class ResponseUpdate(ResponseBase):
