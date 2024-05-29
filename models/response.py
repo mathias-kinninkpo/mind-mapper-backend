b6d9ef96bab939c1c9f98537f2110229ac6ff498
Techteamblt <kinninkpomathias@gmail.com>
2024-05-29 19:59:06 +0100

    init add of models

--- models/response.py
+++ models/response.py
@@ -1,32 +1,22 @@
 # models/response.py
 
-from mongoengine import Document, IntField, StringField, DateTimeField, DictField
+from mongoengine import Document, StringField, DateTimeField, DictField, ReferenceField
 from datetime import datetime
+from .user import User
 
 class Response(Document):
-    id = IntField(primary_key=True, required=True)
-    user_id = IntField(required=True)
+    user_id = ReferenceField(User)
     id_link = StringField(required=True)  # Updated to StringField for UUID
     status = StringField(max_length=255, required=True)
-    id_personality = IntField(required=True)
-    id_statistique = DictField()  # Changed to DictField
+    id_personality = StringField()
+    statistique = DictField()  # Changed to DictField
     date = DateTimeField(default=datetime.utcnow, required=True)
-    content = DictField(required=True)  # Changed to DictField
-    A = StringField(max_length=255)
-    B = StringField(max_length=255)
-    C = StringField(max_length=255)
-    D = StringField(max_length=255)
-    E = StringField(max_length=255)
-    F = StringField(max_length=255)
-    G = StringField(max_length=255)
-    H = StringField(max_length=255)
-    I = StringField(max_length=255)
+    content = DictField(required=True, default=None)  # Changed to DictField
 
     meta = {
         'indexes': [
             'user_id',
             'id_link',
-            'id_personality',
             'date'
         ]
     }
