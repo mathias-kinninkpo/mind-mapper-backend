d6a9ee46aa1aef9ce9587a11f0331ee16c507fd9
Techteamblt <kinninkpomathias@gmail.com>
2024-05-30 03:00:49 +0100

    init add of services

--- services/response.py
+++ services/response.py
@@ -3,29 +3,43 @@
 from typing import List, Optional, Dict
 from uuid import uuid4
 from datetime import datetime
+from fastapi import HTTPException
 from models.response import Response
 from models.user import User
 from schemas.response import ResponseCreate, ResponseOut
 
 def create_response(data: ResponseCreate) -> ResponseOut:
     user =  User.objects(id=data.user_id).first()
+    user_dict = user.to_mongo().to_dict()
+    user_dict['id'] = str(user_dict['_id'])
+    if not user:
+        raise HTTPException(status_code=404, detail="User not found")
+
     response = Response(
         id_link=str(uuid4()),
         status="PENDING",
         date=datetime.utcnow(),
         user_id = user
     )
+    print(response.to_mongo().to_dict())
     response.save()
     response_dict = response.to_mongo().to_dict()
     response_dict['id'] = str(response_dict['_id'])
+    response_dict['user_id'] = user_dict
     return ResponseOut.parse_obj(response_dict)
 
-def get_response(response_id: str) -> Optional[ResponseOut]:
-    response = Response.objects(id=response_id).first()
+def get_response(id_link: str) -> Optional[ResponseOut]:
+    response = Response.objects(id_link=id_link).first()
+    
     if not response:
         return None
     response_dict = response.to_mongo().to_dict()
     response_dict['id'] = str(response_dict['_id'])
+    user = response_dict['user_id']
+    user =  User.objects(id=user).first()
+    user_dict = user.to_mongo().to_dict()
+    user_dict['id'] = str(user_dict['_id'])
+    response_dict['user_id'] = user_dict
     return ResponseOut.parse_obj(response_dict)
 
 def get_all_responses() -> List[ResponseOut]:
@@ -34,16 +48,23 @@ def get_all_responses() -> List[ResponseOut]:
     for response in responses:
         response_dict = response.to_mongo().to_dict()
         response_dict['id'] = str(response_dict['_id'])
+        user = response_dict['user_id']
+        print("user_in =========> ", response_dict['user_id'])
+        print("id", response_dict['id'])
+        user =  User.objects(id=user).first()
+        user_dict = user.to_mongo().to_dict()
+        user_dict['id'] = str(user_dict['_id'])
+        response_dict['user_id'] = user_dict
         result.append(ResponseOut.parse_obj(response_dict))
     return result
 
-def update_response(response_id: str, data: Dict) -> Optional[ResponseOut]:
-    response = Response.objects(id=response_id).first()
+def update_response(id_link: str, data: Dict) -> Optional[ResponseOut]:
+    response = Response.objects(id_link=id_link).first()
     if not response:
         return None
 
     status = data.get("status", response.status)
-    content = data.get("content")
+    content = data.get("content", None)
 
     if content:
         sorted_content = {k: content[k] for k in sorted(content.keys())}
@@ -58,11 +79,16 @@ def update_response(response_id: str, data: Dict) -> Optional[ResponseOut]:
             total_keys = sum(1 for key in content.keys() if key.startswith(letter))
             true_keys = sum(1 for key in content.keys() if key.startswith(letter) and content[key] == True)
             stats[letter] = f"{true_keys}/{total_keys}"
-        response.id_statistique = stats
+        response.statistique = stats
 
     response.save()
     response_dict = response.to_mongo().to_dict()
     response_dict['id'] = str(response_dict['_id'])
+    user = response_dict['user_id']
+    user =  User.objects(id=user).first()
+    user_dict = user.to_mongo().to_dict()
+    user_dict['id'] = str(user_dict['_id'])
+    response_dict['user_id'] = user_dict
     return ResponseOut.parse_obj(response_dict)
 
 def delete_response(response_id: str) -> Optional[ResponseOut]:
@@ -72,6 +98,11 @@ def delete_response(response_id: str) -> Optional[ResponseOut]:
     response.delete()
     response_dict = response.to_mongo().to_dict()
     response_dict['id'] = str(response_dict['_id'])
+    user = response_dict['user_id']
+    user =  User.objects(id=user).first()
+    user_dict = user.to_mongo().to_dict()
+    user_dict['id'] = str(user_dict['_id'])
+    response_dict['user_id'] = user_dict
     return ResponseOut.parse_obj(response_dict)
 
 def update_response_personality(response_id: str, id_personality: int) -> Optional[ResponseOut]:
@@ -82,4 +113,9 @@ def update_response_personality(response_id: str, id_personality: int) -> Option
     response.reload()
     response_dict = response.to_mongo().to_dict()
     response_dict['id'] = str(response_dict['_id'])
+    user = response_dict['user_id']
+    user =  User.objects(id=user).first()
+    user_dict = user.to_mongo().to_dict()
+    user_dict['id'] = str(user_dict['_id'])
+    response_dict['user_id'] = user_dict
     return ResponseOut.parse_obj(response_dict)
